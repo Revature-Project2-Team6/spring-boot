e9c052e15daaae88dbac5fc459695c307ee27f19
Timothy Bautista <yule2000@live.com>
2022-07-08 14:48:59 -0600

    Forgot the @Repository annotation

--- src/main/java/com/revature/data/CharacterRepository.java
+++ src/main/java/com/revature/data/CharacterRepository.java
@@ -3,6 +3,7 @@ package com.revature.data;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.revature.models.Character;
 
@@ -11,6 +12,7 @@ import com.revature.models.Character;
  *
  * @author Teejae Bautista
  */
+@Repository
 public interface CharacterRepository extends JpaRepository<Character, Integer> {
 
     List<Character> findByOwnerId(int id);
