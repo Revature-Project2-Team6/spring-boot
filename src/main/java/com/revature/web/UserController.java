df0637fdb1982cb5aa874a4d179f5df0330a7894
Timothy Bautista <yule2000@live.com>
2022-07-08 16:40:15 -0600

    Begin UserController implementation

---
+++ src/main/java/com/revature/web/UserController.java
@@ -0,0 +1,73 @@
+package com.revature.web;
+
+import java.util.Set;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.revature.models.User;
+import com.revature.service.UserService;
+
+/**
+ * Controller class for <code>User</code>-related actions.
+ *
+ * @author Teejae Bautista
+ */
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/users")
+public class UserController {
+
+    private UserService uServ;
+
+    // Note that declaring @Autowired here is not needed since Spring already
+    // detects this constructor
+    public UserController(UserService uServ) {
+        this.uServ = uServ;
+    }
+
+    /**
+     * Registers a user into the database.
+     *
+     * @param u The <code>User</code> object to register
+     * @return A <code>ResponseEntity</code> object indicating whether or not the
+     *         registration was successful
+     */
+    @PostMapping("/users")
+    public ResponseEntity<User> registerUser(@Valid @RequestBody User u) {
+        return ResponseEntity.ok(this.uServ.add(u));
+    }
+
+    /**
+     * Retrieves all users registered in the database.
+     *
+     * @return A <code>ResponseEntity</code> object containing a <code>Set</code> of
+     *         users found in the database
+     */
+    @GetMapping("/users")
+    public ResponseEntity<Set<User>> getAll() {
+        return ResponseEntity.ok(this.uServ.findAll());
+    }
+
+    /**
+     * Fetches a specific user based on their ID.
+     *
+     * @param id The ID number of the <code>User</code> to get
+     * @return A <code>ResponseEntity</code> object
+     */
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable("id") int id) {
+        return ResponseEntity.ok(this.uServ.getById(id));
+    }
+
+    /**
+     * Starts or continues the game for the logged-in user.
+     */
+    // @GetMapping("/play")
+    // public void playGame() {
+    // // TODO implement method
+    // }
+
+}
