9c783b44afd9e191b57cc1515b819e608c725390
Hector Orozco <hect05661@gmail.com>
2022-07-08 07:19:25 -0700

    Removed regex for email and used annotation instead

--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -39,21 +40,26 @@ public class User {
 	private String password;
 	
 	@NotBlank
-	@Pattern(regexp=RegexUtil.EMAIL_REGEX)
+	@Email
 	@Column(nullable=false, unique=true)
 	private String email;
-
+	
 //	@OneToMany
 //	private List<Character> characters;
 	
+
 	public User(@NotBlank @Length(min = 5) @Pattern(regexp = "\"[a-zA-Z][a-zA-Z0-9]*") String username,
 			@NotBlank @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\\\d)[A-Za-z\\\\d]{8,}$") String password,
-			@NotBlank @Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])") String email) {
+			@NotBlank @Email String email) {
 		super();
 		this.username = username;
 		this.password = password;
 		this.email = email;
 	}
+
+
+	
+
 	
 	
 
